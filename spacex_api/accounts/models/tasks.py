9590964692f04d28c1f0acee21b73affb88438dd
Santiago Rodriguez <santirodriguez38@gmail.com>
2021-03-30 07:55:59 -0300

    Task model altered.

--- spacex_api/accounts/models/tasks.py
+++ spacex_api/accounts/models/tasks.py
@@ -6,6 +6,7 @@ from django.db import models
 # Models
 from spacex_api.accounts.models.base import BaseModel
 from spacex_api.accounts.models.categories import Category
+from spacex_api.users.models import User
 
 
 class Task(BaseModel):
@@ -16,6 +17,8 @@ class Task(BaseModel):
     title = models.CharField(max_length=45)
     description = models.CharField(max_length=255, blank=True)
     category_id = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    trello_list = models.CharField(max_length=140, null=True)
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
