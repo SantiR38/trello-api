b1745495bbbcab828ebd80cb7bf8cc985cf394a5
Santiago Rodriguez <santirodriguez38@gmail.com>
2021-04-03 16:09:59 -0300

    Unnecesary db models deleted.

--- spacex_api/accounts/admin.py
+++ spacex_api/accounts/admin.py
@@ -4,7 +4,7 @@
 from django.contrib import admin
 
 # Models
-from spacex_api.accounts.models import base, categories, tasks
+from spacex_api.accounts.models import base
 
 
 @admin.register(base.Status)
@@ -13,14 +13,3 @@ class StatusAdmin(admin.ModelAdmin):
 
     list_display = ('id', 'name',)
 
-@admin.register(categories.Category)
-class CategoryAdmin(admin.ModelAdmin):
-    """Category model admin."""
-
-    list_display = ('id', 'name',)
-    
-@admin.register(tasks.Task)
-class TaskAdmin(admin.ModelAdmin):
-    """Task model admin."""
-
-    list_display = ('id', 'title', 'description', 'category_id', 'status_id')
